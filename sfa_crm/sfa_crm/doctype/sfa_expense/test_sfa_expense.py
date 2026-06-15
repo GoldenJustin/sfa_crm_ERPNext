984b7bee75047c0842f4c39fd453e2850b674070
GoldenJustin <justinemsengi@gmail.com>
2026-06-15 09:49:34 +0000

    Removing custom doctypes

---
+++ sfa_crm/sfa_crm/doctype/sfa_expense/test_sfa_expense.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Koda Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSFAExpense(FrappeTestCase):
+	pass
