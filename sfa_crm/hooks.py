a57ee38d3ac776303fc02c431075fb9e25a2c4b5
GoldenJustin <justinemsengi@gmail.com>
2026-07-06 04:24:21 +0000

    Version one Complete

--- sfa_crm/hooks.py
+++ sfa_crm/hooks.py
@@ -5,17 +5,16 @@ app_description = "ERPNext App that help to manage Fieldforce SFA"
 app_email = "justinemsengi@gmail.com"
 app_license = "mit"
 
-# required_apps = []
-
-# Allow the mobile app to hit our custom login endpoint without being logged in
+# Allow the mobile app to hit login endpoint without being logged in
 allow_guest_to_call = [
-    "sfa_crm.api.sfa_login"
- "sfa_crm.api.get_site_logo"
+    "sfa_crm.api.sfa_login",
+    "sfa_crm.api.get_site_logo"
 ]
 
-# Include JS/CSS
+# Include CSS
 app_include_css = "/assets/sfa_crm/css/sfa_crm.css"
 
+# Document Events
 doc_events = {
     "Sales Order": {
         "before_validate": "sfa_crm.api.set_default_company"
@@ -27,20 +26,14 @@ doc_events = {
         "before_validate": "sfa_crm.api.set_default_company"
     }
 }
-fixtures = ["Report", "Custom Field", "Property Setter"]
 
-# Fixtures - these get exported to app files via bench export-fixtures
+# Fixtures
 fixtures = [
-    # Client Scripts
     {"dt": "Client Script", "filters": [["dt", "in", [
         "Customer Territory Map",
         "Sales Team Tracker",
         "Salesperson Location Log"
     ]]]},
-
-    # Reports
     {"dt": "Report", "filters": [["module", "=", "Sfa Crm"]]},
-
-    # Custom Doctypes created via UI
     {"dt": "DocType", "filters": [["module", "=", "Sfa Crm"], ["custom", "=", 1]]},
 ]
